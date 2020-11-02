24be672ff13bcdd0bf8461c7c898395d56100cc4
Will Burke <billyburke1012@gmail.com>
2020-11-02 10:41:14 -0500

    updates

---
+++ algorithms/array.py
@@ -0,0 +1,50 @@
+#Given an array nums, write a function to move all zeroes to the end of it while maintaining the relative order of the non-zero elements.
+
+# nums=[0,1,2,3,4]
+# print(testarray)
+
+# def pushZerosToEnd(arr, n): 
+#     count = 0 
+#     for i in range(n): 
+#         if testarray[i] != 0: 
+#             testarray[count] = arr[i] 
+#             count+=1
+
+#     while count < n: 
+#         testarray[count] = 0
+#         count += 1
+
+# n = len(testarray) 
+# pushZerosToEnd(testarray, n) 
+
+ 
+
+# def solution(nums):
+#     if i in nums:
+#         if i == 0:
+#             nums.remove(i)
+#             nums.append(0)
+#         return(nums)
+
+# Write a function that counts the number of times the number 7 occurs in a given integer
+# without converting it to a string.
+# For example the number 7,704,793 would output 3
+
+# test = 7704793
+# print(test)
+# def list_count_7(test):
+#   count = 0  
+#   n = abs(number)
+#   for num in test:
+#     if num == 7:
+#       count = count + 1
+
+#   return count
+
+# print(list_count_7([7704793]))
+
+
+
+
+
+    
