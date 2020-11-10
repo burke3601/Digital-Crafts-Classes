047fe04fa58eaf42d57cae5e6b1df5c1eaf624a9
Will Burke <billyburke1012@gmail.com>
2020-11-10 10:20:03 -0500

    adding homework exercises

--- javascript102/destructuring-assignment.js
+++ javascript102/destructuring-assignment.js
@@ -11,7 +11,7 @@
 
 //rest syntax
 
-let [x,y, ...rest] = [20, 30, 40, 50, 22, 23, 24]
+// let [x,y, ...rest] = [20, 30, 40, 50, 22, 23, 24]
 // console.log(x,y,rest)
 
 //rest is not a keyword, but it is the most popular
@@ -28,3 +28,37 @@ let [x,y, ...rest] = [20, 30, 40, 50, 22, 23, 24]
 // console.log(name)
 
 //Exercise 1
+
+// let [a,b,c,d] = [1,2,3,4]
+
+// console.log(b)
+
+let [,second,,] = [1,2,3,4]
+
+console.log(second)
+
+let[first, ...rest] = [1,2,3,4]
+console.log(first)
+
+//Exercise 2
+
+let ship = 
+    {
+        name:'ship1',
+        age:20,
+        speed:100,
+        acceleration:50
+    }
+    
+
+
+// const {name, age} = ship;
+
+// console.log(name, age)
+//
+const {acceleration} = ship;
+
+console.log(acceleration)
+
+const {age, speed} = ship
+console.log(age, speed)
